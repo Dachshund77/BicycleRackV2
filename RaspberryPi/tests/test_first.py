f19c08c48f8a5ebb1e528f1610542643532f27ed
Sven Büchner <sven.buechner.privat@googlemail.com>
2020-03-15 00:13:47 +0100

    Added final CI

--- RaspberryPi/tests/test_first.py
+++ RaspberryPi/tests/test_first.py
@@ -10,22 +10,21 @@ def test_ok():
     print("ok")
 
 
-def test_fail():
-    assert 0
+# def test_fail():
+#    assert 0
 
 
-def test_error(error_fixture):
-    pass
+# def test_error(error_fixture):
+#    pass
 
 
 def test_skip():
     pytest.skip("skipping this test")
 
 
-def test_xfail():
-    pytest.xfail("xfailing this test")
+# def test_xfail():
+#    pytest.xfail("xfailing this test")
 
-
-@pytest.mark.xfail(reason="always xfail")
-def test_xpass():
-    pass
+# @pytest.mark.xfail(reason="always xfail")
+# def test_xpass():
+#    pass
