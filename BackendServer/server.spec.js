185dd78c91966aa006ae499fa3f977939d3b6342
Sven Büchner <sven.buechner.privat@googlemail.com>
2020-03-14 18:48:14 +0100

    Failure 3

--- BackendServer/server.spec.js
+++ BackendServer/server.spec.js
@@ -3,6 +3,6 @@ let chai = require('chai');
 describe("server Ttests", () => {
 
     it("simple test ", () =>{
-        chai.expect("test").to.eql("test2");
+        chai.expect("test").to.eql("test3");
     });
 });
