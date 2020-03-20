4d9fff3c8381671a0b334ff9207ef40a56d42569
Sven Büchner <sven.buechner.privat@googlemail.com>
2020-03-20 18:54:51 +0100

    Got the registration route bascily right. I hate JS btw

--- BackendServer/models/user.js
+++ BackendServer/models/user.js
@@ -2,8 +2,14 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
 var userSchema = new schema({
-    name: String,
-    password: String
+    name:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
 });
 
 module.exports = mongoose.model('user', userSchema);
--- BackendServer/routes/registrations.js
+++ BackendServer/routes/registrations.js
@@ -4,18 +4,36 @@ var user = require('../models/user');
 
 //Standard registration
 router.route('/')
-    .post(function(request, response) {
-        
-        var newUser = new user(request.body);
+    .post(function (request, response) {
+        console.log('start post');
+        try {
+            //Init values
+            var newUser = new user(request.body);
 
-        newUser.save(function (err) {
-            if(err){
-                console.error(err);
+            //model validation, code 400 bas request
+            var validationError = newUser.validateSync();
+            if (validationError) {
+                response.status(400).json(validationError);
+                return;
             }
-            else{
-                response.status(201).json(newUser);
-            }
-        });
+            console.log('after validation');
+
+            //Insert in db, 500 on error
+            newUser.save(function (dbError) {
+                if (dbError) {
+                    response.status(500).json(dbError);
+                    return;
+                }
+                else {
+                    response.status(201).json(newUser);
+                    return;
+                }
+            });
+
+            //Response created 201
+        } catch (err) {
+            response.status(500).json('Internal server error');
+        }    
     });
 
 module.exports = router;
