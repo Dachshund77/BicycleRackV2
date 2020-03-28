f79136cbc9bfe416b3c2fc736c67212ea442c984
Sven Büchner <sven.buechner.privat@googlemail.com>
2020-03-28 07:00:04 +0100

    Updated model

--- BackendServer/models/device.js
+++ BackendServer/models/device.js
@@ -2,20 +2,25 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
 var deviceSchema = new schema({
+    password: {
+        type: String,
+        required: true
+    },
     ipAdress: {
         type: String,
     },
     location: {
         type: [Number],
-        default: [0,0]
+        default: [0, 0]
     },
     state: {
         type: String,
         default: 'Unknown',
-        enum: ['Occupied', 'Free', 'Error', 'Reserved','unknown'],
+        enum: ['Occupied', 'Free', 'Error', 'Reserved', 'unknown'],
     },
-    lastUpdateReceived:{
+    lastUpdateReceived: {
         type: Date,
+        required: true
     }
 
 });
